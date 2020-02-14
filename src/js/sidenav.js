4a2dcb9c2f3188180de01a8679c63133354db0fa
Mungsul <hukju0714@gmail.com>
2020-02-14 17:00:20 +0900

    fix variable name and split functions

--- src/js/sidenav.js
+++ src/js/sidenav.js
@@ -1,37 +1,59 @@
 var sideNav = document.getElementById("mySidenav")
 var closeBtn = document.getElementsByClassName("convert-tree-closebtn")[0]
 var copyBtn = document.getElementsByClassName("copy-btn")[0]
-closeBtn.onclick = () => {
+var toValue = document.getElementById("toText")
+var tooltipText = document.getElementById("tooltiptext")
+
+function openSideNav() {
+  sideNav.style.width = "250px"
+}
+
+function closeSideNav() {
   sideNav.style.width = "0"
+}
+
+function openTooltip() {
+  tooltipText.style.opacity = 1
+}
+
+function closeTooltip() {
+  tooltipText.style.opacity = 0
+}
+
+function showTooltip() {
+  openTooltip()
+  setTimeout(() => { closeTooltip() }, 2000)
+}
+
+closeBtn.onclick = () => {
+  closeSideNav()
 }
 
 // When the user clicks anywhere outside of the sidenav, close it
 window.onclick = (event) => {
   if (event.target === closeBtn) {
-    sideNav.style.width = "0"
+    closeSideNav()
   }
 }
-
 
 
 window.onkeydown = (event) => {
   if (event.keyCode === 13) {
     if (sideNav.style.width === "250px") {
-      var toValue = document.querySelector("#to-value")
       toValue.select()
       document.execCommand('copy')
+
       sideNav.style.width = "0"
     }
   }
 }
-sideNav.style.width = "250px"
 
 copyBtn.onclick = () => {
-  var copytext = document.getElementById("toText")
-  copytext.focus()
-  copytext.select()
+  toValue.focus()
+  toValue.select()
   document.execCommand("Copy")
-  copytext.blur()
-  document.getElementById("tooltiptext").style.opacity = 1
-  self.setTimeout(() => { document.getElementById("tooltiptext").style.opacity = 0 }, 2000)
+  toValue.blur()
+  showTooltip()
 }
+
+openSideNav()
